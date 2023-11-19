8f49f3195d73c7aa5234272e3233f2ba1657833a
trieutct <96654274+trieutct@users.noreply.github.com>
2023-11-19 21:29:18 +0700

    ......

---
+++ store/src/views/Admin/Dashboard/DoanhThuTheoTuan.vue
@@ -0,0 +1,32 @@
+<template>
+    <canvas id="myChart" style="width:100%"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+export default {
+    mounted() {
+        const ctx = document.getElementById('myChart');
+        new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Triệu', "ádahd"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3, 100, 2],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+}
+</script>
+
+<style></style>
--- store/src/views/Admin/Dashboard/IndexView.vue
+++ store/src/views/Admin/Dashboard/IndexView.vue
@@ -1,39 +1,17 @@
 <template>
     <v-container>
-        <canvas id="myChart" style="width:100%"></canvas>
+        <v-row>
+            <v-col cols="6">
+                <DoanhThuTheoTuan />
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script>
+import DoanhThuTheoTuan from './DoanhThuTheoTuan.vue';
 export default {
-    mounted() {
-        const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-        const yValues = [55, 49, 44, 24, 15];
-        const barColors = [
-            "#b91d47",
-            "#00aba9",
-            "#2b5797",
-            "#e8c3b9",
-            "#1e7145"
-        ];
-
-        new Chart("myChart", {
-            type: "doughnut",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    backgroundColor: barColors,
-                    data: yValues
-                }]
-            },
-            options: {
-                title: {
-                    display: true,
-                    text: "World Wide Wine Production 2018"
-                }
-            }
-        });
-    }
+    components: { DoanhThuTheoTuan }
 }
 </script>
 
